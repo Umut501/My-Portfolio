5d02ba5fd6ac01d37e9cf7db4d3e6431779c4963
Umut Baybece <umut.baybece@kod.com.tr>
2024-01-15 08:20:19 +0300

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,18 +9,19 @@ const Home = () => {
     <section>
     <div className="containerbackg_property py-2" style={{ backgroundImage: `url(${parachutePhoto})`, backgroundSize: 'cover' }}>
       <div className='flex h-screen w-full md:w-auto py-4 items-center justify-center'>
-        {/* <div className="hidden md:flex font-light absolute bottom-20 left left-32">
+       
+        <div className="hidden md:flex font-light absolute bottom-20">
           <svg viewBox="0 0 100 100" width="100" height="100" className="mx-4 animate-spin-slow text-custom-gray">
             <defs>
               <path id="circle" d="M 50, 50 m -37, 0 a 37,37 0 1,1 74,0 a 37,37 0 1,1 -74,0" />
             </defs>
-            <text fontSize={17}>
+            <text fontSize={18}>
               <textPath href="#circle " >
                 <tspan dy="0">Let's get creative & inspiring!</tspan>
               </textPath>
             </text>
           </svg>
-        </div> */}
+        </div>
 
         <h1 className='font-light whitespace-wrap text-9xl md:text-9xl text-center'>
           <span className='font-signature text-center unselectable bg-clip-text text-black'>
