9bcbe39e9f3eddfd769f0ce4a2de53319702b8cb
Umut Baybece <umut.baybece@kod.com.tr>
2024-01-20 17:29:06 +0300

    u

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,12 +2,15 @@
 import React from 'react';
 import './style.css';
 import '../assets/Seth Godin.png';
+import cngguy from '../assets/cngguy.svg';
+import arrow from '../assets/arrow.svg';
+import arrowstraightdown from '../assets/arrowstraightdown.svg';
 
 const Home = () => {
   return (
     <section className="flex flex-col bg-purple cursor-none cd-intro">
       {/* Navigation links */}
-      <div className='flex flex-col items-center md:flex-row whitespace-nowrap justify-end p-4'>
+      <div className='flex flex-col items-end md:flex-row whitespace-nowrap justify-start p-4'>
         <a href="#home" className="text-white px-4">Home</a>
         <a href="#about" className="text-white px-4">About</a>
         <a href="#skills" className="text-white px-4">Skills</a>
@@ -15,7 +18,50 @@ const Home = () => {
         <a href="#projects" className="text-white px-4">Projects</a>
         <a href="#contact" className="text-white px-4">Contact</a>
         <a href="#download" className="text-white px-4">Download CV</a>
-      </div>                                  {/* Copyright (c) 2024 by Juxtopposed (https://codepen.io/Juxtopposed/pen/OJrLZvb)
+      </div>
+
+      <div className='flex flex-row'>
+        {/* Content */}
+        <div className='flex flex-col items-right top-0 p-4 cd-intro-content bouncy'>
+          <div className=' md:py-4 px-4 md:px-32 text-left'>
+            <span className='text-xl mb-4 capitalize text-custom-gray text-custom-header bella'>
+              <span className='whitespace-nowrap text-2xl signature before:block before:absolute before:-inset-6 before:-skew-y-3 relative inline-block'>
+                <span className="relative text-xl text-white">👋Hi I'm Umut </span> <br /><br />
+              </span>
+              <br />
+              <div className="keyboard whitespace-nowrap text-9xl">
+                I'm a&nbsp;
+                <span className='textoutline text-purple whitespace-nowrap'>
+                  <span className="kdey">D</span>
+                  <span className="kdey">E</span>
+                  <span className="kdey">S</span>
+                  <span className="kdey">I</span>
+                  <span className="kdey">G</span>
+                  <span className="kdey">N</span>
+                  <span className="kdey">E</span>
+                  <span className="kdey">R</span>
+                </span>
+                <span className='Rubik text-xl'><b> &</b></span>
+              </div>
+              <span className='text-9xl'>Developer</span><br /><img className='hidden xl:block px-20' alt="Meaningful description" src={arrowstraightdown} />
+
+              <div className='invisible md:visible w-9/12 flex justify-start items-center p-4'>
+                <img src={cngguy} alt="cngguy" className='w-full' />
+                {/* https://www.streamlinehq.com/ bu siteyi vereceğim credit olarak yeterli */}
+              </div>
+
+
+            </span>
+          </div>
+        </div>
+
+
+        {/* Cngguy Image */}
+        <div className='invisible py-10 xl:visible flex justify-start items-center'>
+          <img src={require('../assets/Seth Godin.png')} alt="Seth Godin" className='w-12/12' />  </div>
+
+      </div>
+      {/* Copyright (c) 2024 by Juxtopposed (https://codepen.io/Juxtopposed/pen/OJrLZvb)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
@@ -23,37 +69,8 @@ The above copyright notice and this permission notice shall be included in all c
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */}
-
-      {/* Content */}
-      <div className='flex flex-col items-left p-4 cd-intro-content bouncy'>
-        <div className='py-8 md:py-16 px-4 md:px-32 text-left'>
-          <span className='text-xl mb-4 capitalize text-custom-gray text-custom-header bella'>
-            <span className='whitespace-nowrap text-2xl signature before:block before:absolute before:-inset-6 before:-skew-y-3 relative inline-block'>
-              <span className="relative text-xl text-white">👋Hi I'm Umut </span><br />
-              <br />
-            </span>
-            <br />
-            <div className="keyboard whitespace-nowrap text-9xl">
-              I'm a&nbsp;
-              <span className='textoutline text-purple whitespace-nowrap'>
-                <span className="kdey">D</span>
-                <span className="kdey">E</span>
-                <span className="kdey">S</span>
-                <span className="kdey">I</span>
-                <span className="kdey">G</span>
-                <span className="kdey">N</span>
-                <span className="kdey">E</span>
-                <span className="kdey">R</span>
-              </span>
-              <span className='Rubik text-xl'><b> &</b></span>
-            </div>
-            <span className='text-9xl'>Developer</span>
-            <img src={require('../assets/Seth Godin.png')} alt="Seth Godin" 
-        className='w-5/12'/>
-          </span>
-        </div>
-      </div>
     </section>
+
   );
 };
 
